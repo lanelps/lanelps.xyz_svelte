7589b520b094fb7b26c78bd7f2e70f00cbfd47d6
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-02-03 14:03:46 +1300

    added tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			gray: {
+				100: `#f5f5f5`
+			}
+		},
+		extend: {}
+	},
+	plugins: []
+};
