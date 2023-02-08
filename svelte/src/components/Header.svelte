52d1a9395b9a7b045e4887268252bdd8e654a3c2
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-02-09 10:57:07 +1300

    Layout styling

---
+++ svelte/src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	const links = [
+		{ id: 1, url: `/`, name: `Home` },
+		{ id: 2, url: `/about`, name: `About` },
+		{ id: 3, url: `/work`, name: `Work` },
+		{ id: 4, url: `/contact`, name: `Contact` }
+	];
+</script>
+
+<header class="absolute top-0 w-full px-3 z-50">
+	<nav>
+		<ul class="w-full flex gap-x-10 border-b py-3">
+			{#each links as link (link.id)}
+				<li class="font-main text-main">
+					<a href={link?.url}>{link?.name}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
+
+<style>
+</style>
--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../styles/index.css';
+	import Header from '../components/Header.svelte';
 </script>
 
-<slot />
+<div
+	class="fixed inset-0 w-[calc(100vw-2.5rem)] h-[calc(100vh-1.5rem)] mx-5 my-3 border overflow-hidden"
+>
+	<Header />
+
+	<main class="px-3 pt-[3.75rem]">
+		<slot />
+	</main>
+</div>
