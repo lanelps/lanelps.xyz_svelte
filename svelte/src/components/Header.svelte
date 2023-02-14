f11d94f0e808422f8ceefaa3d33c65778e499ab9
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-02-15 12:29:15 +1300

    mobile nav

--- svelte/src/components/Header.svelte
+++ svelte/src/components/Header.svelte
@@ -5,10 +5,13 @@
 		{ id: 3, url: `/work`, name: `Work` },
 		{ id: 4, url: `/contact`, name: `Contact` }
 	];
+
+	let menuActive = false;
+	let mobileRef: HTMLElement;
 </script>
 
-<header class="absolute top-0 w-full px-3 z-50">
-	<nav>
+<header class="absolute top-0 w-full px-3 bg-white md-t:bg-transparent z-50">
+	<nav class="hidden md-t:block">
 		<ul class="w-full flex gap-x-10 border-b py-3">
 			{#each links as link (link.id)}
 				<li class="font-main text-main">
@@ -16,6 +19,37 @@
 				</li>
 			{/each}
 		</ul>
+	</nav>
+
+	<nav class="block md-t:hidden">
+		<div class="border-b">
+			<div>
+				<button
+					class="py-3 font-main text-main"
+					class:active={menuActive}
+					on:click={() => (menuActive = !menuActive)}
+				>
+					{#if menuActive}
+						Close
+					{:else}
+						Menu
+					{/if}
+				</button>
+			</div>
+
+			<nav
+				class="transition-[height] duration-300 overflow-hidden"
+				style="height: {menuActive ? mobileRef?.clientHeight : 0}px"
+			>
+				<ul bind:this={mobileRef} class="w-full flex flex-col gap-y-3 pb-3">
+					{#each links as link (link.id)}
+						<li class="font-main text-main">
+							<a href={link?.url}>{link?.name}</a>
+						</li>
+					{/each}
+				</ul>
+			</nav>
+		</div>
 	</nav>
 </header>
 
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -7,10 +7,12 @@
 	export let data: HomePageData;
 </script>
 
-<section class="grid grid-cols-3 gap-x-4">
-	<div class="sticky top-0 h-max flex flex-col gap-y-6">
+<section class="grid grid-cols-3 gap-4">
+	<div
+		class="relative md-t:sticky md-t:overflow-y-scroll md-t:top-0 md-t:h-max col-span-full md-t:col-span-1 flex flex-col gap-y-6"
+	>
 		<RichText value={data?.body} />
 	</div>
 
-	<Media media={data?.media} class="col-span-2" loading="eager" />
+	<Media media={data?.media} class="w-full col-span-full md-t:col-span-2" loading="eager" />
 </section>
