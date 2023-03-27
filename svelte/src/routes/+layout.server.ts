96ab05c1e08706bf193b9af8ff06cdb4388c1bb7
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-03-27 20:23:28 +1300

    layout url fix

--- svelte/src/routes/+layout.server.ts
+++ svelte/src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ export const prerender = true;
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
-	console.log(`url`, url);
 	return {
 		url: url.pathname
 	};
